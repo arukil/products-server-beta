fd14eb4ea5fc74c4bb930b39094150f5fbce8b3f
arukil <arukilt20@gmail.com>
2020-09-15 19:19:15 +0530

    fix

--- controller/user/signin.js
+++ controller/user/signin.js
@@ -5,6 +5,7 @@ const User = require('../../model/user/signin')
 const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 
 const jwt = require('jsonwebtoken');
+const { FieldValueList } = require('twilio/lib/rest/autopilot/v1/assistant/fieldType/fieldValue');
 
 
 exports.phonenumberSignin = async (req, res) => {
@@ -17,7 +18,7 @@ exports.phonenumberSignin = async (req, res) => {
             return res.json({ message: true });
         })
         .catch(err => {
-            return res.json({ message: `Enter number is invalid ${process.env.SERVICEID}`, error: err });
+            return res.json({ message: false });
         })
 
 };
@@ -34,7 +35,7 @@ exports.phonenumberVerify = async (req, res) => {
             const isUserExisting = await User.findOne({ phonenumber: req.body.phonenumber });
             if (isUserExisting) {
                 const token = await jwt.sign({ userId: isUserExisting._id, }, process.env.JWTSECRET, { expiresIn: '180d' });
-                return res.json({ verification: true, token: token, user: isUserExisting, expiresIn: 15552000 });
+                return res.json({ message:true, token: token, user: isUserExisting, expiresIn: 15552000 });
             }
             else {
                 const user = await new User({
@@ -43,15 +44,15 @@ exports.phonenumberVerify = async (req, res) => {
                 });
                 user.save().then(async () => {
                     const token = await jwt.sign({ userId: user._id, }, process.env.JWTSECRET, { expiresIn: '180d' });
-                    return res.json({ verification: true, token: token, user: user, expiresIn: 15552000 });
+                    return res.json({ message: true, token: token, user: user, expiresIn: 15552000 });
                 })
                     .catch(() => {
-                        return res.json({ message: "something went wrong,try again later" });
+                        return res.json({ message: "something went wrong,try agaipn later" });
                     });
             }
         })
         .catch((err) => {
-            return res.status(404).json({ error: err, message: 'please enter valid otp' });
+            return res.status(404).json({ message: false });
         });
 
 };
